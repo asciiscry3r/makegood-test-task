821d5cee09691f67fced5f683e8e3e3003ee9dc2
Max <klimenkomaximsergievich@gmail.com>
2021-05-15 23:11:51 +0300

    Added provisioner

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,22 @@ mkdir -p certbot/{conf,www}
 
 git submodule update --init --recursive
 
+if [ ! -f "/usr/local/bin/docker-compose" ]; then
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+fi
+
 if [ -d NodeJS ]; then
-	cd NodeJS && docker build -t makegood-nodejs . && cd ..
+	cd NodeJS && sudo docker build -t makegood-nodejs . && cd ..
 else
 	echo "Sorry but we lost our NodeJS files"
 fi
 
 if [ -d nginx ]; then
-	cd nginx-docker && docker build -t makeggod-nginx . && cd ..
+	cd nginx-docker && sudo docker build -t makeggod-nginx . && cd ..
 else
 	echo "Sorry but we lost our Nginx files"
 fi
 
-docker-compose up -d
+sudo docker-compose up -d
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,33 @@ terraform {
   }
 }
 
+##################### VARIABLES #########################
+
 variable "zone_id" {
   default = "Z08690643G2MCG6F1C5D7"
+}
+
+variable "dns_name" {
+  default = "makegood.mksplayground.online"
+}
+
+variable "private_key_path" {
+  default = "/home/max/.ssh/makegood"
+}
+
+variable "public_key_content" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJwRyDnkNifjja1K52u18Sw5ohD63ypTS36A3Kfvq4W8qMvYBAVXwb8Ympah6Pd+xHFzwiJLUdAdk0SzsCpOayQjQ4wwHsbQX0FD01cZM9aYSaWla5sylUeckYATo7bktVUA7GJVUKO3G7LbiNrWSSlouKRuM+OWOYS9nkKzWy1Kd0rODPHeKr5axMHe1l7bCCpfk6+Bduoi2K9YawF1RL08Aujmv5a7N24NwMBiUHPCRRRTFgb/FduuucMIQ2Psv60tCsuNE7NXWNVbv5EfZwKGR3kY0RibAcqp8U2morom18w3b2T3BTshE4YVHedcisWa0e5B08hnuaCguVHFw4xndaNUEWMdgRsmy49jqkb7XPTjXIOuRnbpcqLZeM/12uVWwsGqQkufPwa+s6ZDRnPRf8J7xZM2uFqicJKgl1/q0Q8dTUjdneTBgm0i2NHReZzndLQAIjdApEXrGaTdIxrmlQgHJ2lEAE1QytRLeUcg4mecljHzDTsp7FAKvbUeE= max@mksideapad"
+}
+
+variable "my_ip_address" {
+  default = ["108.61.39.2/32"]
 }
 
 variable "vpc_id_main" {
   default = "vpc-5b018826"
 }
+
+#########################################################
 
 data "aws_vpc" "selected" {
   id = var.vpc_id_main
@@ -25,7 +45,7 @@ provider "aws" {
 
 resource "aws_key_pair" "makegood-key" {
   key_name   = "makegood-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJwRyDnkNifjja1K52u18Sw5ohD63ypTS36A3Kfvq4W8qMvYBAVXwb8Ympah6Pd+xHFzwiJLUdAdk0SzsCpOayQjQ4wwHsbQX0FD01cZM9aYSaWla5sylUeckYATo7bktVUA7GJVUKO3G7LbiNrWSSlouKRuM+OWOYS9nkKzWy1Kd0rODPHeKr5axMHe1l7bCCpfk6+Bduoi2K9YawF1RL08Aujmv5a7N24NwMBiUHPCRRRTFgb/FduuucMIQ2Psv60tCsuNE7NXWNVbv5EfZwKGR3kY0RibAcqp8U2morom18w3b2T3BTshE4YVHedcisWa0e5B08hnuaCguVHFw4xndaNUEWMdgRsmy49jqkb7XPTjXIOuRnbpcqLZeM/12uVWwsGqQkufPwa+s6ZDRnPRf8J7xZM2uFqicJKgl1/q0Q8dTUjdneTBgm0i2NHReZzndLQAIjdApEXrGaTdIxrmlQgHJ2lEAE1QytRLeUcg4mecljHzDTsp7FAKvbUeE= max@mksideapad"
+  public_key = var.public_key_content
 }
 
 resource "aws_security_group" "makegood-allow" {
@@ -39,7 +59,6 @@ resource "aws_security_group" "makegood-allow" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = [data.aws_vpc.selected.ipv6_cidr_block]
   }
 
   ingress {
@@ -47,8 +66,7 @@ resource "aws_security_group" "makegood-allow" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["108.61.39.2/32"]
-#    ipv6_cidr_blocks = [data.aws_vpc.selected.ipv6_cidr_block]
+    cidr_blocks      = var.my_ip_address
   }
 
   egress {
@@ -56,32 +74,43 @@ resource "aws_security_group" "makegood-allow" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "makegood_allow_tls_and access"
+    Name = "makegood_allow_tls_and_ssh_access"
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "makegood-ec2" {
-  ami                    = "ami-04cd519d2f9578053"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.makegood-key.key_name
-  iam_instance_profile   = "arn:aws:iam:::role/makegood-ec2-access-to-route53"
+  iam_instance_profile   = "makegood-ec2-access-to-route53"
   vpc_security_group_ids = [aws_security_group.makegood-allow.id]
 
-  provisioner "remote-exec" {
-    inline = [
-      "git clone https://github.com/asciiscry3r/makegood-test-task.git makegood",
-      "cd makegood",
-      "chmod +x bootstrap.sh",
-      "./bootstrap.sh",
-    ]
+  root_block_device {
+     delete_on_termination = true
+     volume_size           = 10
   }
 
   tags = {
-    Name = "makegood"
+    Name = "makegood-ec2"
   }
 }
 
@@ -92,9 +121,30 @@ resource "aws_eip" "makegood-eip" {
 
 resource "aws_route53_record" "makegood-www" {
   zone_id = var.zone_id
-  name    = "makegood.mksplayground.online"
+  name    = var.dns_name
   type    = "A"
   ttl     = "300"
   records = [aws_eip.makegood-eip.public_ip]
+}
+
+resource "null_resource" "cluster" {
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -fsSL https://get.docker.com -o get-docker.sh",
+      "sudo sh get-docker.sh",
+      "sudo rm -rf makegood && git clone https://github.com/asciiscry3r/makegood-test-task.git makegood",
+      "cd makegood",
+      "chmod +x bootstrap.sh",
+      "./bootstrap.sh",
+    ]
+
+    connection {
+       type        = "ssh"
+       user        = "ubuntu"
+       private_key = file(var.private_key_path)
+       host        = var.dns_name
+    }
+  }
 }
 
